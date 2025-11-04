7390e57e7fc2fdc029481f7b58ec0d786ec500c6
Miquel Paredes <maparedesp14@gmail.com>
2025-11-04 10:18:38 +0100

    Episode 35 creacio del container

--- Dynamic_Web_Applications/public/index.php
+++ Dynamic_Web_Applications/public/index.php
@@ -11,7 +11,7 @@ spl_autoload_register(function ($class){
     require base_path("{$class}.php");
 });
 
-require base_path('Core/Router.php');
+require base_path('bootstrap.php');
 
 $router = new \Core\Router();
 
