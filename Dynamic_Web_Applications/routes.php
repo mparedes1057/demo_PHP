0c17a5620a97087d4cf27b8b61f33526c61139b8
Miquel Paredes <maparedesp14@gmail.com>
2025-11-04 09:19:17 +0100

    Episode 33 refactoritzacio de les rutes

--- Dynamic_Web_Applications/routes.php
+++ Dynamic_Web_Applications/routes.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 return [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
@@ -8,3 +8,13 @@ return [
     '/notes/create' => 'controllers/notes/create.php',
     '/contact' => 'controllers/contact.php'
 ];
+*/
+$router -> get('/', 'controllers/index.php');
+$router -> get('/about', 'controllers/about.php');
+$router -> get('/notes', 'controllers/notes/index.php');
+
+$router -> get('/note', 'controllers/notes/show.php');
+$router -> get('/notes/create', 'controllers/notes/create.php');
+$router -> get('/contact', 'controllers/contact.php');
+
+//$router -> delete('/nota', 'controllers/notes/destroy.php');
